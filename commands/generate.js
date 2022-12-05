c80fbf5842a397bd72cc5411264d64d0fa5b868f
busybox11 <busybox11th@gmail.com>
2022-12-05 21:44:48 +0100

    bot: Working generate command

---
+++ commands/generate.js
@@ -0,0 +1,47 @@
+const https = require('https');
+const fs = require('fs');
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('generate')
+		.setDescription('Generates a video with the specified duration in seconds')
+        .addStringOption(option =>
+            option.setName('message_id')
+                .setDescription('ID of the message that contains the picture to use'))
+        .addStringOption(option =>
+            option.setName('duration')
+                .setDescription('Target duration of the output video in seconds')),
+
+	async execute(client, interaction) {
+        interaction.deferReply();
+        const channel = client.channels.cache.get(interaction.channelId)
+        await channel.messages.fetch(interaction.options.getString('message_id'))
+            .then(async (message) => {
+                if (!fs.existsSync('generated')){
+                    fs.mkdirSync('generated');
+                }
+
+                const file = fs.createWriteStream("generated/file.jpg");
+                https.get(message.attachments.first().url, function(response) {
+                    response.pipe(file);
+
+                    // after download completed close filestream
+                    file.on("finish", async () => {
+                        file.close()
+                        await exec(`ffmpeg -y -r 30 -i generated/file.jpg -t 20 -c:v libx265 -x265-params lossless=1 -pix_fmt yuv420p -vf "scale=1080:1920,loop=-1:1" -movflags faststart generated/file.jpg.mp4`)
+                        await interaction.followUp('Video generated')
+                        
+                        const attachment = new AttachmentBuilder('./generated/file.jpg.mp4', { name: 'output.mp4' })
+                        await interaction.editReply({ files: [attachment] })
+                        
+                        const interactionMsgObj = await interaction.fetchReply()
+                        await interaction.followUp(interactionMsgObj.attachments.first().url)
+                    });
+                });
+            })
+            .catch(console.error);
+	},
+}
---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
+
+// Create a new client instance
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+] })
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`)
+})
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(client, interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.BOT_TOKEN)
